81f05a83b06fd193b3e851c8d1ba5c2781cf5130
Danil Siver <ubermensch.dev@gmail.com>
2020-06-22 20:04:47 +0300

    Showed json usage, added watch mode

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 // Importing model to show, that it goes to the final bundle
 import Post from "./Post";
 import "./styles/styles.css";
+import json from "./assets/json.json";
  
 const post = new Post('My new awesome title');
 
 console.log('Post to string: ', post.toString());
+
+console.log('JSON', json);
